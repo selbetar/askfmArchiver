dc6cb27271b1260ec087df06c03cfc5a22530970
selbetar <sadeqhsb@tuta.io>
2021-09-18 22:23:40 -0700

    Refactor: Use Dependecy Injection
    - Use the dependency injection design pattern.

--- askfmArchiver/Program.cs
+++ askfmArchiver/Program.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using askfmArchiver.Enums;
+using askfmArchiver.Models;
 using askfmArchiver.Utils;
 using CommandLine;
-using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using Options = askfmArchiver.Utils.Options;
 
 namespace askfmArchiver
 {
@@ -14,64 +20,79 @@ namespace askfmArchiver
     {
         private static async Task Main(string[] args)
         {
-            var dbTask = CreateTables();
 
-            string userId = "", pageIterator = "", input = "", output = "";
-            var stopAt = DateTime.Now;
-            var type = JobType.NONE;
-
+            var archive = false;
+            IHost host = null;
             CommandLine.Parser.Default.ParseArguments<Options>(args)
-                .WithParsed(opts =>
+                .WithParsed(options =>
                 {
-                    userId = opts.UserId.ToLower();
-                    pageIterator = opts.PageIterator;
-                    stopAt = opts.StopAt;
-                    input = opts.Input;
-                    output = opts.Output;
+                    options.UserId = options.UserId.ToLower();
+                    var builder = BuildConfig(options);
 
-                    var tmp = opts.Type.ToLower();
-                    switch (tmp)
-                    {
-                        case "p":
-                        case "parse":
-                            type = JobType.PARSE;
-                            break;
-                        case "m":
-                        case "md":
-                        case "markdown":
-                            type = JobType.MARKDOWN;
-                            break;
-                    }
+                    Log.Logger = new LoggerConfiguration()
+                        .ReadFrom.Configuration(builder.Build())
+                        .Enrich.FromLogContext()
+                        .WriteTo.Console()
+                        .WriteTo.File("log_askfm.txt", LogEventLevel.Information)
+                        .CreateLogger();
+
+                    host = Host.CreateDefaultBuilder()
+                        .ConfigureServices((context, services) =>
+                        {
+                            services.AddSingleton<IFileManager, FileManager>();
+                            services.AddSingleton<IOptions>(options);
+                            services.AddDbContext<MyDbContext>(opts =>
+                                opts.UseSqlite(context.Configuration.GetConnectionString("DefaultConnection")));
+                            if (options.Archive)
+                            {
+                                services.AddTransient<IParser, Parser>();
+                                services.AddSingleton<INetworkManager, NetworkManager>();
+                                archive = true;
+                            }
+                            else
+                            {
+                                services.AddTransient<IMarkDown, MarkDown>();
+                            }
+                        })
+                        .UseSerilog()
+                        .Build();
+
                 })
                 .WithNotParsed(HandleParseError);
-            CommandLine.Parser.Default.ParseArguments<Options>(args);
 
-            var fm = new FileManager();
-            fm.CheckDir(output);
-
-            var logFileName = "Log_" + userId + "_" + DateTime.Now.ToString("yy-MM-dd_HH-mm").
-                Replace("-", "") + ".txt";
-            var logFile = Path.Combine(output, logFileName);
-            Logger.SetLogFile(logFile);
-
-            await dbTask;
-            switch (type)
+            if (archive)
             {
-                case JobType.PARSE:
-                    var askfmParser = new Parser(userId, output, pageIterator, stopAt);
-                    await askfmParser.Parse();
-                    break;
-                case JobType.MARKDOWN:
-                    var markDown = new MarkDown(userId, output);
-                    await markDown.Generate();
-                    break;
-                case JobType.NONE:
-                    Logger.WriteLine("Error in specified type. Run --help for supported job types.");
-                    Environment.Exit(1);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException();
+                Log.Logger.Information("Application is Starting: Archival Service");
+                var svc = ActivatorUtilities.CreateInstance<Parser>(host.Services);
+                await svc.Parse();
+                Log.Logger.Information("Application is Done: Archival Service");
             }
+            else
+            {
+                Log.Logger.Information("Application is Starting: Markdown Service");
+                var svc = ActivatorUtilities.CreateInstance<MarkDown>(host.Services);
+                await svc.Generate();
+                Log.Logger.Information("Application is Done: Markdown Service");
+            }
+        }
+
+        private static ConfigurationBuilder BuildConfig(Options options)
+        {
+            var builder = new ConfigurationBuilder();
+            options.Output = options.Output == ""
+                ? Path.Combine(Directory.GetCurrentDirectory(), "output")
+                : Path.GetFullPath(options.Output);
+
+            Directory.CreateDirectory(options.Output);
+
+            var env = Environment.GetEnvironmentVariable("DOTNET_ENVIRONMENT") ?? Environments.Development;
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env}.json",
+                    optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables();
+
+            return builder;
         }
 
         private static void HandleParseError(IEnumerable<Error> errs)
@@ -84,67 +105,14 @@ namespace askfmArchiver
                     Console.WriteLine(error.ToString());
                 }
             }
+
             Environment.Exit(1);
         }
 
-        private static async Task CreateTables()
-        {
-            var con = new SqliteConnectionStringBuilder()
-            {
-                DataSource = @"data.db",
-                RecursiveTriggers = true,
-                ForeignKeys = true
-            }.ToString();
-
-            await using var connection = new SqliteConnection(con);
-            string[] queries =
-            {
-                @"CREATE TABLE IF NOT EXISTS Users(
-	                UserID TEXT PRIMARY KEY,
-	                UserName TEXT,
-	                LastQuestion DateTime DEFAULT 0,
-	                FirstQuestion DateTime DEFAULT 0)"
-                ,
-                @"CREATE TABLE IF NOT EXISTS Answers (
-					UserID TEXT NOT NULL,
-   					AnswerID TEXT NOT NULL,
-   					AnswerText TEXT,
-					QuestionText TEXT NOT NULL,
-					AuthorID TEXT,
-					AuthorName TEXT,
-					Date DateTime NOT NULL,
-					Likes INTEGER NOT NULL,
-					VisualID  TEXT,
-					VisualType INTEGER,
-					VisualUrl TEXT,
-					VisualExt TEXT,
-					VisualHash TEXT,
-					ThreadID TEXT NOT NULL,
-					PageID TEXT,
-					PRIMARY KEY (AnswerID),
-				    FOREIGN KEY (UserID) 
-				        REFERENCES Users (UserID) 
-				            ON DELETE CASCADE 
-				            ON UPDATE NO ACTION)"
-                ,
-                @"CREATE TABLE IF NOT EXISTS PDFGen (UserID TEXT, StopAt DateTime, AnswerID Text, PRIMARY KEY (UserID) 
-				FOREIGN KEY (UserID)
-        		REFERENCES Users (UserID))",
-            };
-
-            connection.Open();
-            var command = connection.CreateCommand();
-            var tasks = new List<Task<int>>();
-            foreach (var query in queries)
-            {
-                command.CommandText = query;
-                var tsk = command.ExecuteNonQueryAsync();
-                tasks.Add(tsk);
-            }
-
-            await Task.WhenAll(tasks);
-            command.Dispose();
-            connection.Close();
-        }
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                    services.AddDbContext<MyDbContext>(opts =>
+                        opts.UseSqlite(context.Configuration.GetConnectionString("DefaultConnection"))));
     }
 }
